8ca301c701ef903fd9c3b87594da6aae0a170028
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-12-02 16:19:56 +0000

    Updated to latest version

--- list_emails.py
+++ list_emails.py
@@ -3,16 +3,16 @@ import dbm
 confirmed = list()
 unconfirmed = list()
 
-with dbm.open("passwords.dbm") as db:
+with dbm.open('passwords.dbm', 'r') as db:
     with dbm.open('registration.dbm', 'r') as reg:
         with dbm.open('email.dbm', 'r') as emails:
             username = db.firstkey()
             while username != None:
                 if username in emails:
                     if username in reg:
-                        unconfirmed.append(emails[username])
+                        unconfirmed.append((username, emails[username]))
                     else:
-                        confirmed.append(emails[username])
+                        confirmed.append((username, emails[username]))
                 username = db.nextkey(username)
 
 print("Confirmed")
